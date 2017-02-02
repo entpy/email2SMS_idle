69dec5c063ef661cd3b1ae624c9523db6af04285
developer <developer@entpy.com>
2017-02-02 15:12:40 +0100

    code cleaning

--- email2SMS_idle/email2SMS_idle/settings.py
+++ email2SMS_idle/email2SMS_idle/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '6bc%%u%7^=a%7f7e=gd9xxpjhlhc4t%5(7-ebvtj49+st%ty=&'
+SECRET_KEY = 'SECRET_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -74,12 +74,14 @@ WSGI_APPLICATION = 'email2SMS_idle.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+"""
 
 
 # Password validation
